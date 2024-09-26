f18f28025e4cbab60153845db1e0bee23000ecdc
Kidus Gebru <kidusgebruhagos@gmail.com>
2024-09-26 06:48:33 -0700

    3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,11 +6,12 @@ import requests
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers
     to the subreddit"""
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit),
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+            .format(subreddit),
                             headers={"User-Agent": "PostmanRuntime/7.42.0"})
 
     if sub_info.status_code == 200:
         return sub_info.json().get("data").get("subscribers")
     else:
         return 0
-
